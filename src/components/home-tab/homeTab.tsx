3a36be2f619df2c6cb608db7501cfefc576fc3c9
Ade <johnmaskretro@gmail.com>
2024-08-24 12:32:17 +0100

    updated telegram checker

--- src/components/home-tab/homeTab.tsx
+++ src/components/home-tab/homeTab.tsx
@@ -50,7 +50,7 @@ const HomeTab = () => {
 
         {
             id: 2,
-            name: "Follow us on X",
+            name: "Follow on X",
             reward: 150,
             to: "https://twitter.com/FitcoinEarn",
             btnText: "Follow",
@@ -81,17 +81,7 @@ const HomeTab = () => {
             icon: <svg xmlns="http://www.w3.org/2000/svg" fill="#FFFFFF" width="20px" viewBox="0 0 512 512"><path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z" /></svg>
 
 
-        },
-        {
-            id: 5,
-            name: "Invite 5 frens",
-            reward: 5000,
-            to: "https://twitter.com/FitcoinEarn",
-            btnText: "Start",
-            icon: <svg xmlns="http://www.w3.org/2000/svg" fill="#FFFFFF" width="20px" viewBox="0 0 512 512"><path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z" /></svg>
-
-
-        },
+        }
 
 
 
