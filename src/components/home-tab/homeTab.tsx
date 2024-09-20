b0cf5f8923c9893afac8e1fa5d5685023d053afd
Ade <johnmaskretro@gmail.com>
2024-09-20 15:16:38 +0100

    updated story func

--- src/components/home-tab/homeTab.tsx
+++ src/components/home-tab/homeTab.tsx
@@ -1040,7 +1040,7 @@ const HomeTab = () => {
                             <button
                                 className="w-full flex bg-gradient-to-r my-4 mx-auto items-center justify-center from-[#F19D5C] to-[#F0E580] font-OpenSans text-lg text-black rounded-lg  py-2 rounded-[1px]"
 
-                                onClick={() => handleShareToStory('https://example.com/image.jpg', 'My Caption')}
+                                onClick={() => handleShareToStory('https://asset.cloudinary.com/doyovxikn/9089c9b4911224a6d1bc71d518142a0a', 'My Caption')}
                                 
 
                             >
